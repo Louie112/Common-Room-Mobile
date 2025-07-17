2739dcd1adbeae10f5e637e83bdf6b5953d56043
Louie <rosepolecat@outlook.com>
2025-07-17 02:02:42 -0400

    key redacted

--- configs/firebaseConfig.tsx
+++ configs/firebaseConfig.tsx
@@ -1,12 +1,12 @@
 import { initializeApp, getApps, getApp } from '@react-native-firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA5rMibKDXf-z47NclGzN8Os8llRQdY64Q',
-  authDomain: 'louiepersonalfirebase.firebaseapp.com',
-  projectId: 'louiepersonalfirebase',
-  storageBucket: 'louiepersonalfirebase.appspot.com',
-  messagingSenderId: '172287347189',
-  appId: '1:172287347189:android:d287e5a0e5dddb8355f8bc',
+  apiKey: 'redacted',
+  authDomain: 'redacted',
+  projectId: 'redacted',
+  storageBucket: 'redacted',
+  messagingSenderId: 'redacted',
+  appId: 'redacted',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
